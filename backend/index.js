16f6ab37c130fe71ccd45f9205e6efba5772f38e
harshit-506 <supriyamohapatra658@gmail.com>
2024-05-15 23:57:02 +0530

    setting up mongoDB for storage

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
-const express=require("express")
-const cors=require("cors")
-require("dotenv").config()
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./database/connection.js";
+import Data from "./models/data.js";
+
+
+
 
 const app=express()
-
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true
+  }));
+  app.use(express.json())
 const port=process.env.PORT || 8000
+
+connectDB()
+
+app.post("/api/data",async(req,res)=>{
+  try {
+    const jsonData=req.body;
+    const data=new Data(jsonData)
+    await data.save()
+    res.status(201).json({ message: 'Data saved successfully', data });
+  } catch (error) {
+    console.error('Error saving data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+})
+
+
 
 app.listen(port,()=>{
     console.log(`Server live at ${port}`)
---
+++ backend/models/data.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const dataSchema = new mongoose.Schema({
+    end_year: { type: String, default: '' },
+    intensity: { type: Number, default: ''  },
+    sector: { type: String,default: ''  },
+    topic: { type: String,  default: '' },
+    insight: { type: String, default: ''  },
+    url: { type: String,  default: '' },
+    region: { type: String, default: '' },
+    start_year: { type: String, default: '' },
+    impact: { type: String, default: '' },
+    added: { type: Date, default: ''  },
+    published: { type: Date,default: ''   },
+    country: { type: String, default: ''  },
+    relevance: { type: Number,default: ''  },
+    pestle: { type: String, default: '' },
+    source: { type: String,default: '' },
+    title: { type: String, default: ''  },
+    likelihood: { type: Number,default: ''  }
+}, { timestamps: true });
+
+const Data = mongoose.model('Data', dataSchema);
+
+export default Data;
