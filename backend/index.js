908bd036a05a62051c8069c1c85663aa6543bcad
harshit-506 <supriyamohapatra658@gmail.com>
2024-05-15 22:08:25 +0530

    setting up backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const cors=require("cors")
+require("dotenv").config()
+
+const app=express()
+
+const port=process.env.PORT || 8000
+
+app.listen(port,()=>{
+    console.log(`Server live at ${port}`)
+})
+
